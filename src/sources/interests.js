6ed1ec27fa42d364b4cd51ac8eec100d08918a26
David van de Vondervoort <dd1528@L3118-Mac.local>
2016-06-23 17:12:18 +1200

    Update default data model

--- src/sources/interests.js
+++ src/sources/interests.js
@@ -1,10 +1,10 @@
 import uuid from 'uuid';
 
 const interests = [
-  { id: uuid.v4(), text: 'React', importance: 0},
-  { id: uuid.v4(), text: 'Angular 2', importance: 0},
-  { id: uuid.v4(), text: 'Ember', importance: 0},
-  { id: uuid.v4(), text: 'NodeJS', importance: 0},
+  { id: uuid.v4(), text: 'React', relevance: 0, siblings: ['Redux', 'Router']},
+  { id: uuid.v4(), text: 'Angular 2', relevance: 0, siblings: ['ngrx', 'Typescript', 'Observables']},
+  { id: uuid.v4(), text: 'Ember', relevance: 0, siblings: []},
+  { id: uuid.v4(), text: 'NodeJS', relevance: 0, siblings: ['Express']},
 ];
 
 export default interests;
