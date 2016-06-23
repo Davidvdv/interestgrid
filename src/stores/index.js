535fdd2acf218a113419c5b38fd1d7c303cac148
David van de Vondervoort <dd1528@L3118-Mac.local>
2016-06-23 17:10:46 +1200

    Store subscribes to changes for when there store changes

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,11 +3,16 @@ import {syncHistoryWithStore} from 'react-router-redux';
 import {browserHistory} from 'react-router';
 import rootReducer from '../reducers/index';
 import interests from '../sources/interests';
+import {saveState, loadState} from './localStorage';
+import throttle from 'lodash.throttle';
 
-const defaultState = {
-  interests
-};
+const defaultState = loadState() ? loadState() : {interests};
 
 const store = createStore(rootReducer, defaultState, window.devToolsExtension && window.devToolsExtension());
 export const history = syncHistoryWithStore(browserHistory, store);
+
+store.subscribe(throttle(() => {
+  saveState(store.getState());
+}), 1000);
+
 export default store;
